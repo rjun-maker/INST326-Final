e62b497112d43e568e59a66c6c7caf6538357412
Abe740 <abe.uzb.1998@gmail.com>
2025-04-08 17:33:19 -0400

    Update PracticingGit.py
    Added the variables that I will use in my algorithm

--- PracticingGit.py
+++ PracticingGit.py
@@ -3,6 +3,9 @@ This is a practice python file group members can hoepfully look at and edit
 
 """
 
+ROUNDS = 10  # Abe's variable 
+SATISFACTION_RATING = 80 # Abe's variable
+
 list = ['panda', 'giraffe', 'penguin', 'shark', 'dragon', 'snake'] # Renee's test list
 
 #Aric is a cool guy!
