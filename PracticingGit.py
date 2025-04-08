5fa2150cc9e98a82495b7641b483b0dc23aa28f4
Renee Jun <rjun@terpmail.umd.edu>
2025-04-08 14:31:10 -0400

    Aric is a cool guy!

--- PracticingGit.py
+++ PracticingGit.py
@@ -4,3 +4,5 @@ This is a practice python file group members can hoepfully look at and edit
 """
 
 list = ['panda', 'giraffe', 'penguin', 'shark', 'dragon', 'snake'] # Renee's test list
+
+#Aric is a cool guy!
