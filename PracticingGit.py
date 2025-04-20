e490c839ddaa0ded063b14a2df1528a055cfa1c1
Renee Jun <rjun@terpmail.umd.edu>
2025-04-20 15:09:52 -0400

    Git commit test

--- PracticingGit.py
+++ PracticingGit.py
@@ -1,16 +1,31 @@
 """ 
-This is a practice python file group members can hoepfully look at and edit
+Zoo keeper simulator!
 
 """
 
-ROUNDS = 10  # Abe's variable 
-SATISFACTION_RATING = 80 # Abe's variable
+""" 
+function that will determine a randomly generated scenario the result will
+pop out an animal event.
 
-list = ['panda', 'giraffe', 'penguin', 'shark', 'dragon', 'snake'] # Renee's test list
+"""
 
-#Aric is a cool guy!
+import random # choose a random index
 
-print("testudo is a turtle") # Aiden's change
+def my_function(scenario):
+    print(scenario)
+    
+scenariolist = [] # should I use a list? Or a diff container data set??
+    
+my_function("You walk up to the panda bear exhibit and see that a baby panda \
+             is crying. Do you A. Give it a banana or B. Punch it")
 
-#hello
-#ademir's change
+userresponse = input("Type A or B")
+
+if userresponse == "B":
+    print("You clicked B ")
+    
+elif userresponse == "A":
+    print("You clicked A ")
+    
+else:
+    print("Sorry please retry it.")
