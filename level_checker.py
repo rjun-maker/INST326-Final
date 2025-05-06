a3f64759b621e472463306c79b91d0e5c5a1f3c5
Aiden Anderson <aja061@umd.edu>
2025-05-06 16:21:07 -0400

    made win condition function

--- level_checker.py
+++ level_checker.py
@@ -3,11 +3,24 @@
 # they win
 # what's needed:
 # - way to check level
-# - way to check how long the eplayer has been at a certain level
+# - way to check how long the player has been at a certain level
 # - way to associate staying at the level with winning the game
+
+
+# checks level, 5 rounds = 1 level, will move on to next level
+# win condition is level 10 for 5 rounds
 
 def level_checker(current_level, previous_level, same_level_rounds):
     """Tracks how many rounds a person have been at a particular level
+    
+    Args:
+        current_level (int): current level of the player
+        previous_level (int): level of the player in the former round
+        same_level_rounds (int): how many rounds the player has done at their
+        current level
+    
+    Returns:
+        int: update count of rounds at player's current level
     """
     
     if current_level == previous_level:
@@ -19,4 +32,24 @@ def level_checker(current_level, previous_level, same_level_rounds):
     return same_level_rounds
 
 
+def win_condition(current_level, duration):
+    """determines in player wins by staying at max level for 5 rounds
+    
+    Args:
+        current_level (int): player's current level
+        same_level_rounds (int): how many rounds the player has stayed at the
+        current level
+    
+    Returns
+        bool: Returns true if player is  at level 10 for 5 rounds
+    """
+    return current_level == 10 and duration >= 5
+
 print(level_checker(2, 2, 5))
+
+WIN_GOAL = 5
+duration = 4
+
+if win_condition(duration, WIN_GOAL):
+    print("You win")
+    
